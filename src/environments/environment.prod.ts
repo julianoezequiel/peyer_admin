5e609d9d54028d3541574013ceb515a3be69e003
Juliano Cesar Ezequiel <juliano.ezequiel@topdata.com.br>
2022-02-17 16:55:14 -0300

    01

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyCYY23JJOPk4Zt58QRhvVtltx7o07SjNAs",
-    authDomain: "organicossaojose-19c51.firebaseapp.com",
-    databaseURL: "https://organicossaojose-19c51.firebaseio.com",
-    projectId: "organicossaojose-19c51",
-    storageBucket: "organicossaojose-19c51.appspot.com",
-    messagingSenderId: "105269105226",
-    appId: "1:105269105226:web:61d5c358c11c5cc4e88cb3",
-    measurementId: "G-WP9WZ179B3"
+  firebase: {
+    apiKey: 'AIzaSyCPtCJhFemhJxqahikRWsJSW2XeiJNQpZ4',
+    authDomain: 'peyer-e1597.firebaseapp.com',
+    projectId: 'peyer-e1597',
+    storageBucket: 'peyer-e1597.appspot.com',
+    messagingSenderId: '503831674152',
+    appId: '1:503831674152:web:f006c463f1b54e0085e41d',
+    measurementId: 'G-9NCTL4YBVT'
   }
-};
+}
